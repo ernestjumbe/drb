b2a6e80398135d70131188bef9d6c3fd969c92c1
ernestjumbe <e2jeyy@gmail.com>
2015-06-27 01:13:21 +0200

    Included port number

--- setup/dev_gunicorn_config.py
+++ setup/dev_gunicorn_config.py
@@ -1,6 +1,6 @@
 command = '/opt/drbenv/bin/gunicorn'
 pythonpath = '/opt/drbenv/detrundebord'
-bind = '46.101.255.172'
+bind = '46.101.255.172:8001'
 logfile = "/var/www/drb/logs/gunicorn.log"
 workers = 3
 user = 'nobody'

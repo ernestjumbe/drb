133e2a2b6211811e5d446b881108f29555b98ad9
ernestjumbe <e2jeyy@gmail.com>
2015-06-27 15:54:57 +0200

    Tabulated inlines for dishes.

--- apps/dishes/admin.py
+++ apps/dishes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Dish, Ingredient
 
-class IngredientInline(admin.StackedInline):
+class IngredientInline(admin.TabularInline):
 	model = Ingredient
 	raw_id_fields = ("product",)
 	extra = 1

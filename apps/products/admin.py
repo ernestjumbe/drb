9730d78e406fd43e94062f7f6b7c737a679f29c5
ernestjumbe <e2jeyy@gmail.com>
2015-06-27 17:01:29 +0200

    Added quantity field and made current weight uneditable.

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -4,6 +4,7 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
 	list_display = ('id', 'name', 'product_type', 'status')
 	list_display_links = ('name',)
+	readonly_fields=('current_weight',)
 
 
 admin.site.register(Product, ProductAdmin)

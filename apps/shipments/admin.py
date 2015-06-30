25089e30aa101d8d26a3d1baf3bf8e8a0d49bc29
ernestjumbe <e2jeyy@gmail.com>
2015-06-30 20:54:10 +0200

    Shipment auto select user .

--- apps/shipments/admin.py
+++ apps/shipments/admin.py
@@ -19,4 +19,26 @@ class ShipmentAdmin(admin.ModelAdmin):
 	list_display = ('id', 'destination', 'departure', 'driver')
 	list_display_links = ('destination',)
 
+	
+	def formfield_for_foreignkey(self, db_field, request, **kwargs):
+		if db_field.name == 'user':
+			kwargs['queryset'] = User.objects.filter(username=request.user.username)
+		return super(ShipmentAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+	
+	def get_readonly_fields(self, request, obj=None):
+		if obj is not None:
+			return self.readonly_fields + ('user',)
+		return self.readonly_fields
+	
+	# def get_fields(self, request, obj=None):
+	# 	if obj is not None:
+	# 		return self.fields + (('created', 'modified'),)
+	# 	return self.fields
+	
+	def add_view(self, request, form_url="", extra_context=None):
+		data = request.GET.copy()
+		data['user'] = request.user
+		request.GET = data
+		return super(ShipmentAdmin, self).add_view(request, form_url="", extra_context=extra_context)
+
 admin.site.register(Shipment, ShipmentAdmin)

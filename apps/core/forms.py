c0ae858e12c3ebf84213bc38af5f117dc0b22dc7
ernestjumbe <e2jeyy@gmail.com>
2015-07-03 08:19:07 +0200

    Login dialouge for non-existant users.

--- apps/core/forms.py
+++ apps/core/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth import authenticate
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 
@@ -12,7 +13,17 @@ class LoginForm(forms.Form):
 	                           attrs={'placeholder':_('Password')}))
 
 	def __init__(self, *args, **kwargs):
-	    super(LoginForm, self).__init__(*args, **kwargs)
-	    self.helper = FormHelper()
-	    self.helper.form_method = 'post'
-	    self.helper.add_input(Submit('submit', _('Sign In')))
+		super(LoginForm, self).__init__(*args, **kwargs)
+		self.helper = FormHelper()
+		self.helper.form_method = 'post'
+		self.helper.add_input(Submit('submit', _('Sign In')))
+
+	def clean(self):
+		username = self.cleaned_data.get('username')
+		password = self.cleaned_data.get('password')
+		
+		if username and password:
+			user = authenticate(username=username, password=password)
+			if user is None:
+				raise forms.ValidationError(_('Please enter correct username and password.'))
+		return self.cleaned_data
